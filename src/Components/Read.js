2f0d584a515b8dbd7fde338981bd0696c26577d4
savita <pokhriyal.savi777@gmail.com>
2023-10-16 19:14:00 +0530

    crud initial

---
+++ src/Components/Read.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Read = () => {
+  const [data, setData] = useState([]);
+  const [tabledark, setTableDark] = useState("");
+
+  function getData() {
+    axios
+      .get("https://651bca0f194f77f2a5aedcbb.mockapi.io/crud-1")
+      .then((res) => {
+        setData(res.data);
+      });
+  }
+
+  function handleDelete(id) {
+    axios
+      .delete(`https://651bca0f194f77f2a5aedcbb.mockapi.io/crud-1/${id}`)
+      .then(() => {
+        getData();
+      });
+  }
+
+  const setToLocalStorage = (id, name, email) => {
+    localStorage.setItem("id", id);
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div className="form-check form-switch">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          onClick={() => {
+            if (tabledark === "table-dark") setTableDark("");
+            else setTableDark("table-dark");
+          }}
+        />
+      </div>
+      <div className="d-flex justify-content-between">
+        <h2>Read Operation</h2>
+        <Link to="/">
+          <button className="btn btn-secondary">Create</button>
+        </Link>
+      </div>
+      <table className={`table ${tabledark}`}>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        {data.map((eachData, key) => {
+          return (
+            <tbody key={key}>
+              <tr>
+                <th scope="row">{eachData.id}</th>
+                <td>{eachData.name}</td>
+                <td>{eachData.email}</td>
+                <td>
+                  <Link to="/update">
+                    <button
+                      className="btn-success"
+                      onClick={() =>
+                        setToLocalStorage(
+                          eachData.id,
+                          eachData.name,
+                          eachData.email
+                        )
+                      }
+                    >
+                      Edit{""}
+                    </button>
+                  </Link>
+                </td>
+                <td>
+                  <button
+                    className="btn-danger"
+                    onClick={() => handleDelete(eachData.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+    </>
+  );
+};
+
+export default Read;
